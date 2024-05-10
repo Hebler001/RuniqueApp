8abec8a325e2e365369be3b3bd0ee06fdb521414
JHebler <158784807+Hebler001@users.noreply.github.com>
2024-05-10 15:59:44 -0500

    Create material theme

--- run/location/build.gradle.kts
+++ run/location/build.gradle.kts
@@ -7,13 +7,8 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.kotlinx.coroutines.core)
 
     implementation(projects.core.domain)
     implementation(projects.run.domain)
