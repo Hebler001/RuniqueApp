845854ab1cf7c19e6337973bcb1dd5432170bf63
JHebler <158784807+Hebler001@users.noreply.github.com>
2024-05-26 23:18:16 -0500

    Create the register screen UI

--- auth/domain/src/main/java/com/jhebler/auth/domain/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.jhebler.auth.domain
-
-class MyClass {
-}
---
+++ auth/domain/src/main/java/com/jhebler/auth/domain/UserDataValidator.kt
@@ -0,0 +1,9 @@
+package com.jhebler.auth.domain
+
+class UserDataValidator {
+
+    companion object {
+        const val MIN_PASSWORD_LENGTH = 9
+    }
+
+}
