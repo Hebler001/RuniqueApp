045c1ee51ad31458640b326bfc5d05e738e7da35
JHebler <158784807+Hebler001@users.noreply.github.com>
2024-06-01 16:23:44 -0500

    Setup token refresh mechanism

---
+++ core/data/src/main/java/com/jhebler/core/data/networking/AccessTokenRequest.kt
@@ -0,0 +1,9 @@
+package com.jhebler.core.data.networking
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AccessTokenRequest(
+    val refreshToken: String,
+    val userId: String
+)
---
+++ core/data/src/main/java/com/jhebler/core/data/networking/AccessTokenResponse.kt
@@ -0,0 +1,9 @@
+package com.jhebler.core.data.networking
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AccessTokenResponse(
+    val accessToken: String,
+    val expirationTimestamp: String
+)
