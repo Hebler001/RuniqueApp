878ae0af640cfa30501e2317f4c202ebb5ea94a3
JHebler <158784807+Hebler001@users.noreply.github.com>
2024-06-02 17:55:14 -0500

    Setup login functionality

--- auth/data/src/main/java/com/jhebler/auth/data/AuthRepositoryImpl.kt
+++ auth/data/src/main/java/com/jhebler/auth/data/AuthRepositoryImpl.kt
@@ -2,13 +2,38 @@ package com.jhebler.auth.data
 
 import com.jhebler.auth.domain.AuthRepository
 import com.jhebler.core.data.networking.post
+import com.jhebler.core.domain.AuthInfo
+import com.jhebler.core.domain.SessionStorage
 import com.jhebler.core.domain.util.DataError
 import com.jhebler.core.domain.util.EmptyResult
+import com.jhebler.core.domain.util.Result
+import com.jhebler.core.domain.util.asEmptyDataResult
 import io.ktor.client.HttpClient
 
 class AuthRepositoryImpl(
-    private val httpClient: HttpClient
+    private val httpClient: HttpClient,
+    private val sessionStorage: SessionStorage
 ): AuthRepository {
+
+    override suspend fun login(email: String, password: String): EmptyResult<DataError.Network> {
+        val result = httpClient.post<LoginRequest, LoginResponse>(
+            route = "/login",
+            body = LoginRequest(
+                email = email,
+                password = password
+            )
+        )
+        if(result is Result.Success) {
+            sessionStorage.set(
+                AuthInfo(
+                    accessToken = result.data.accessToken,
+                    refreshToken = result.data.refreshToken,
+                    userId = result.data.userId
+                )
+            )
+        }
+        return result.asEmptyDataResult()
+    }
 
     override suspend fun register(email: String, password: String): EmptyResult<DataError.Network> {
         return httpClient.post<RegisterRequest, Unit>(
--- auth/domain/src/main/java/com/jhebler/auth/domain/AuthRepository.kt
+++ auth/domain/src/main/java/com/jhebler/auth/domain/AuthRepository.kt
@@ -4,6 +4,6 @@ import com.jhebler.core.domain.util.DataError
 import com.jhebler.core.domain.util.EmptyResult
 
 interface AuthRepository {
-
+    suspend fun login(email: String, password: String): EmptyResult<DataError.Network>
     suspend fun register(email: String, password: String): EmptyResult<DataError.Network>
 }
