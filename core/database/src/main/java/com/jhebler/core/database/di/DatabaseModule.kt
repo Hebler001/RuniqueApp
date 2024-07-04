743750029ca6726ef19f0b029f7d28e92d38d92e
JHebler <158784807+Hebler001@users.noreply.github.com>
2024-07-03 22:24:29 -0500

    Save pending syncs

--- core/database/src/main/java/com/jhebler/core/database/di/DatabaseModule.kt
+++ core/database/src/main/java/com/jhebler/core/database/di/DatabaseModule.kt
@@ -3,6 +3,7 @@ package com.jhebler.core.database.di
 import androidx.room.Room
 import com.jhebler.core.database.RoomLocalRunDataSource
 import com.jhebler.core.database.RunDatabase
+import com.jhebler.core.database.dao.RunPendingSyncDao
 import com.jhebler.core.domain.run.LocalRunDataSource
 import org.koin.android.ext.koin.androidApplication
 import org.koin.core.module.dsl.singleOf
@@ -18,5 +19,6 @@ val databaseModule = module {
         ).build()
     }
     single { get<RunDatabase>().runDao }
+    single { get<RunDatabase>().runPendingSyncDao }
     singleOf(::RoomLocalRunDataSource).bind<LocalRunDataSource>()
 }
